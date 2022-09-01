af85e71479dff2c2f72c523e7db37fc9fcf71d9d
yuyang <yuyang545262477@gmail.com>
2022-09-01 23:38:40 +0800

    Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 app.get('/', function(req, res) {
- res.send('Response String');
+ res.send('Hello Express');
 })
 console.log('Hello World');
 
